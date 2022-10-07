0c555e3dce0bc76ae4babe9709e24fd5b61af8f8
USER <USER@DESKTOP-TRQG01D>
2022-10-07 13:37:24 +0300

    Добавьте файлы проекта.

---
+++ Pr6.Zd.1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x,S ;
+            Console.WriteLine("Введите значение переменной x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            double Pi = 3.1415926535;
+            Console.WriteLine("Значение Pi равно: {0}", Pi);
+            S = x - Pi;
+            Console.WriteLine("Разность x-Pi равна: {0}",Math.Round (S,8));
+            Console.ReadLine();
+           
+        }
+    }
+}
